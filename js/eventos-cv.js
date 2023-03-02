b9266f70ae629d66875fe1a0e13bcbfcc6d7bb97
matiballe3000 <126629771+matiballe3000@users.noreply.github.com>
2023-03-01 22:14:26 -0300

    Add files via upload

---
+++ js/eventos-cv.js
@@ -0,0 +1,34 @@
+document.getElementById('boton-email').onclick = function(){
+    document.getElementById("nombre").innerHTML= "Mi email es Mati.mati@gmail.com"
+}
+
+document.getElementById('boton-nacimiento').onclick = function(){
+
+    document.getElementById("nombre").innerHTML= "Fecha de nacimiento 14/3/1988"+ " " + " "+ "DNI: 654632"
+    
+}
+
+document.getElementById('boton-telefono').onclick = function(){
+    document.getElementById("nombre").innerHTML= "Telefono de contacto: 654321896"
+}
+
+document.getElementById('boton-nombre').onclick = function(){
+    document.getElementById("nombre").innerHTML= "Mi nombre es: Matias Ballesteros"
+   
+}
+
+
+  function bigImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+  }
+  function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+  }
+  
+  ocument.getElementById('sobremi').onclick = function(){
+
+    document.getElementById("nombre").innerHTML= "Fecha de nacimiento 14/3/1988"+ " " + " "+ "DNI: 654632"
+    
+}
